acef4d566f4cc7b7d422d4be228a33159fd9c340
sean galloway <sean.galloway@outlook.com>
2021-02-05 10:41:11 -0800

    code cleanup

--- static/js/logic.js
+++ static/js/logic.js
@@ -2,6 +2,9 @@
 const earthquakeUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 const plateUrl = 'https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json';
 
+var categories = ['0','1', '2', '3', '4','5','6','7+'];
+var categories_color = [0, 1, 2, 3, 4, 5, 6, 7];
+
 // Perform a GET request to the query URL
 (async function(){
   const earthquakeData = await d3.json(earthquakeUrl);
@@ -10,17 +13,18 @@ const plateUrl = 'https://raw.githubusercontent.com/fraxen/tectonicplates/master
   const plateData = await d3.json(plateUrl);
   console.log(plateData.features)
 
-  // Once we get a response, send the data.features objects to the createFeatures function
+  // Once we get a response, send the *.features objects to the createFeatures function
   createFeatures(earthquakeData.features, plateData.features);
 })()
 
 function getColor(magnitude) {
-  return  magnitude > 5  ?  '#ee6c6e' :
-          magnitude > 4  ?  '#eea770' :
-          magnitude > 3  ?  '#f2b957' :
-          magnitude > 2  ?  '#f2db5a' :
-          magnitude > 1  ?  '#e2f15b' :
-                            '#b8f15a' ;
+  return  magnitude >= 7  ? '#ee6c6e' :
+          magnitude >= 6  ? '#eea770' :
+          magnitude >= 5  ? '#f2b957' :
+          magnitude >= 3  ? '#f2db5a' :
+          magnitude >= 2  ? '#adff2f' :
+          magnitude >= 1  ? '#008000' :
+                            '#006400' ;
 }
 
 function createFeatures(earthquakeData, plateData) {
@@ -28,7 +32,7 @@ function createFeatures(earthquakeData, plateData) {
   // Give each feature a popup describing the place, magnitude and time of the earthquake
   function onEachFeature(feature, layer) {
     layer.bindPopup("<h3>" + feature.properties.place + "</h3>" +
-    "Magnitude: <strong>" + feature.properties.mag + "</strong><hr>" +
+      "Magnitude: <strong>" + feature.properties.mag + "</strong><hr>" +
       "<p>" + new Date(feature.properties.time) + "</p>");
   }
 
@@ -37,9 +41,9 @@ function createFeatures(earthquakeData, plateData) {
   var earthquakes = L.geoJSON(earthquakeData, {
     pointToLayer: function(feature, latlng) {
       return new L.CircleMarker(latlng, {
-          radius: feature.properties.mag * 5,
+          radius: feature.properties.mag * 3,
           fillColor: getColor(feature.properties.mag),
-          fillOpacity: 1.0,
+          fillOpacity: 0.8,
           weight: 1,
           color: "black"
       });
@@ -62,28 +66,28 @@ function createMap(earthquakes, plates) {
     maxZoom: 18,
     id: 'mapbox/satellite-streets-v11',
     accessToken: API_KEY
-});
+  });
 
-const grayscale = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+  const grayscale = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     id: "mapbox/light-v10",
     accessToken: API_KEY
-});
+  });
 
-const outdoors = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+  const outdoors = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     id: "mapbox/outdoors-v11",
     accessToken: API_KEY
-});
+  });
 
-// Define a baseMaps object to hold our base layers
-const baseMaps = {
+  // Define a baseMaps object to hold our base layers
+  const baseMaps = {
     "Satellite": satellite,
     "Grayscale": grayscale,
     "Outdoors": outdoors
-};
+  };
 
   // Create overlay object to hold our overlay layer
   var overlayMaps = {
@@ -117,13 +121,9 @@ const baseMaps = {
   legend.onAdd = function (myMap) {
     let div = L.DomUtil.create('div', 'info legend');
     var labels = ['<strong>Magnitude</strong>'];
-    var categories = ['0-1','1-2','2-3','3-4','4-5','5+'];
-    var categories_color = [0.5, 1.5, 2.5, 3.5, 4.5, 5.5];
-
     for (var i = 0; i < categories.length; i++) {
         div.innerHTML +=
-          labels.push(
-                        '<i class="circle" style="background:' + getColor(categories_color[i]) + '"></i> ' +
+          labels.push('<i class="circle" style="background:' + getColor(categories_color[i]) + '"></i> ' +
                         (categories[i] ? categories[i] : '+'));
     }
     div.innerHTML = labels.join('<br>');
